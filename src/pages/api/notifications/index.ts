ddb2d7978b06c3ec589751875cf9867911ae2c73
Álvaro Ferreira <alvaro.ferreira@amcom.com.br>
2023-08-15 15:55:51 -0300

    fix: remote cron

--- src/pages/api/notifications/index.ts
+++ src/pages/api/notifications/index.ts
@@ -1,160 +1,152 @@
 import type { NextApiResponse } from 'next';
 import { Schedule } from '../../../services/database';
-import { withIronSessionApiRoute } from 'iron-session/next';
 import moment from 'moment';
 import { getWhatsappInstance, startSession } from '@/services/whatsapp';
 import { transformPhoneNumber } from '@/utils/general';
 import { remaindMessage } from '@/utils/notificarions';
 
-export default withIronSessionApiRoute(
-  async function handler(
-    req: any,
-    res: NextApiResponse
-  ) {
-    try {
-      if (req.method === 'GET') {
-        //Busca os atendimentos que precisam de notificação
-        const schedules = await Schedule.aggregate([
-          {
-            $addFields: {
-              day: {
-                $dateToString: {
-                  format: '%Y-%m-%d',
-                  date: '$date',
-                },
+export default async function handler(
+  req: any,
+  res: NextApiResponse
+) {
+  try {
+    if (req.method === 'GET') {
+      const schedules = await Schedule.aggregate([
+        {
+          $addFields: {
+            day: {
+              $dateToString: {
+                format: '%Y-%m-%d',
+                date: '$date',
               },
             },
           },
-          {
-            $match: {
-              day: moment().format('YYYY-MM-DD'),
-              status: 'agendado'
-            },
+        },
+        {
+          $match: {
+            day: moment().format('YYYY-MM-DD'),
+            status: 'agendado'
           },
-          {
-            $lookup: {
-              from: 'services',
-              let: { serviceIds: '$serviceIds' },
-              pipeline: [
-                {
-                  $match: {
-                    $expr: { $in: ['$_id', '$$serviceIds'] },
-                  },
+        },
+        {
+          $lookup: {
+            from: 'services',
+            let: { serviceIds: '$serviceIds' },
+            pipeline: [
+              {
+                $match: {
+                  $expr: { $in: ['$_id', '$$serviceIds'] },
                 },
-              ],
-              as: 'services',
-            },
+              },
+            ],
+            as: 'services',
           },
-          {
-            $lookup: {
-              from: 'professionals',
-              localField: 'professionalId',
-              foreignField: '_id',
-              as: 'professional',
-            },
+        },
+        {
+          $lookup: {
+            from: 'professionals',
+            localField: 'professionalId',
+            foreignField: '_id',
+            as: 'professional',
           },
-          {
-            $unwind: '$professional',
+        },
+        {
+          $unwind: '$professional',
+        },
+        {
+          $lookup: {
+            from: 'clients',
+            localField: 'clientID',
+            foreignField: '_id',
+            as: 'client',
           },
-          {
-            $lookup: {
-              from: 'clients',
-              localField: 'clientID',
-              foreignField: '_id',
-              as: 'client',
-            },
-          },
-          {
-            $unwind: '$client',
-          },
-          {
-            $lookup: {
-              from: 'companies',
-              localField: 'companyId',
-              foreignField: '_id',
-              as: 'company',
-              pipeline: [
-                {
-                  $match: {
-                    isWhatsappService: true
-                  },
+        },
+        {
+          $unwind: '$client',
+        },
+        {
+          $lookup: {
+            from: 'companies',
+            localField: 'companyId',
+            foreignField: '_id',
+            as: 'company',
+            pipeline: [
+              {
+                $match: {
+                  isWhatsappService: true
                 },
-              ]
-            },
+              },
+            ]
           },
-          {
-            $unwind: '$company',
-          },
-        ]);
+        },
+        {
+          $unwind: '$company',
+        },
+      ]);
 
 
-        const countChanged = 0
+      const countChanged: any = []
 
-        const toNotify: any[] = []
+      const toNotify: any[] = []
 
 
-        schedules.forEach((schedule) => {
-          const diff = moment(schedule.time, 'hh:mm').diff(moment(), 'minutes')
-          if (diff > 60 && diff < 120) {
-            toNotify.push(schedule)
-          }
-        })
-
-        const groupsByCompanyId: any = {};
-        for (const object of toNotify) {
-          const companyId = object.companyId;
-          if (!groupsByCompanyId[companyId]) {
-            groupsByCompanyId[companyId] = [];
-          }
-          groupsByCompanyId[companyId].push(object);
+      schedules.forEach((schedule) => {
+        const diff = moment(schedule.time, 'hh:mm').diff(moment(), 'minutes')
+        if (diff > 60 && diff < 120) {
+          toNotify.push(schedule)
         }
+      })
 
-
-        for (const companyId in groupsByCompanyId) {
-          const group = groupsByCompanyId[companyId];
-          try {
-            console.log(`Send mensage to: ${group[0].company.name}`);
-            const startSessionRes = await startSession(group[0].company)
-            if (!startSessionRes) return
-            for (const schedule of group) {
-              await getWhatsappInstance(group[0].company.whatsappToken).post(
-                `${process.env.NEXT_PUBLIC_WHATSAPP_SERVICE_API
-                }/api/${transformPhoneNumber(group[0].company.whatsapp || group[0].company.whatsapp)}/send-message`,
-                {
-                  phone: transformPhoneNumber(schedule.client.phone),
-                  isGroup: false,
-                  message: remaindMessage(schedule),
-                }
-              );
-              console.log('Sent notification to: ', schedule.client.name);
-            }
-            console.log('All notifications from ', group[0].company.name, 'was sent.');
-          } catch (error) {
-            console.log('Error to send some notification')
-          } finally {
-            await getWhatsappInstance(group[0].company.whatsappToken).post(
-              `${process.env.NEXT_PUBLIC_WHATSAPP_SERVICE_API
-              }/api/${transformPhoneNumber(group[0].company.whatsapp || group[0].company.whatsapp)}/close-session`,
-              {}
-            );
-          }
+      const groupsByCompanyId: any = {};
+      for (const object of toNotify) {
+        const companyId = object.companyId;
+        if (!groupsByCompanyId[companyId]) {
+          groupsByCompanyId[companyId] = [];
         }
-
-        return res.status(200).send(countChanged)
+        groupsByCompanyId[companyId].push(object);
       }
 
-      return res.status(404);
-    } catch (error: any) {
-      console.log(error);
-      return res.status(500).json({ data: error, message: error.message });
+
+      for (const companyId in groupsByCompanyId) {
+        const group = groupsByCompanyId[companyId];
+        try {
+          console.log(`Send mensage to: ${group[0].company.name}`);
+          const startSessionRes = await startSession(group[0].company)
+          if (!startSessionRes) return
+          for (const schedule of group) {
+            await getWhatsappInstance(group[0].company.whatsappToken).post(
+              `${process.env.NEXT_PUBLIC_WHATSAPP_SERVICE_API
+              }/api/${transformPhoneNumber(group[0].company.whatsapp || group[0].company.whatsapp)}/send-message`,
+              {
+                phone: transformPhoneNumber(schedule.client.phone),
+                isGroup: false,
+                message: remaindMessage(schedule),
+              }
+            );
+            countChanged.push({
+              company: schedule.company.name,
+              clientName: schedule.client.name,
+            })
+            console.log('Sent notification to: ', schedule.client.name);
+          }
+          console.log('All notifications from ', group[0].company.name, 'was sent.');
+        } catch (error) {
+          console.log('Error to send some notification')
+        } finally {
+          await getWhatsappInstance(group[0].company.whatsappToken).post(
+            `${process.env.NEXT_PUBLIC_WHATSAPP_SERVICE_API
+            }/api/${transformPhoneNumber(group[0].company.whatsapp || group[0].company.whatsapp)}/close-session`,
+            {}
+          );
+        }
+      }
+
+      return res.status(200).send(countChanged)
     }
-  },
-  {
-    cookieName: 'doupi_cookie',
-    //@ts-ignore
-    password: process.env.SESSION_SECRET,
-    cookieOptions: {
-      secure: process.env.NODE_ENV === 'production',
-    },
+
+    return res.status(404);
+  } catch (error: any) {
+    console.log(error);
+    return res.status(500).json({ data: error, message: error.message });
   }
-);
+}
